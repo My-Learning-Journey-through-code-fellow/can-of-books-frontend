0a02e0a937bd70ba50674d4841f70a832f08323f
Trace Dugar <tracedugar89@gmail.com>
2022-10-31 18:17:15 -0700

    attempting to get backend to communicate with frontend

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,4 +1,6 @@
+import axios from 'axios';
 import React from 'react';
+import App from './App';
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -8,10 +10,23 @@ class BestBooks extends React.Component {
     }
   }
 
+  getBooks = async () => {
+    try {
+      let bookData = await axios.get(`${process.env.DB_URL}/books`)
+      this.setState({
+        books: bookData.data,
+      })
+    } catch (error) {
+      console.log('We have an error.', error.response);
+    }
+  }
+  componentDidMount() {
+    this.getBooks();
+  }
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
 
   render() {
-
+    console.log("App State >>>", this.state);
     /* TODO: render all the books in a Carousel */
 
     return (
