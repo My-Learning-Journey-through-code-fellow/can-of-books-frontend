d9f390566f09e84db29382c3ae4bb3d4e9398edc
Raphael Chookagian <raphaelchookagian@gmail.com>
2022-11-01 08:44:07 -0700

    adding and trying to fix the header and nav on frontend

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ class Footer extends React.Component {
   render() {
     return (
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand>Code Fellows</Navbar.Brand>
+        <Navbar.Brand>Code Fellows 2022</Navbar.Brand>
       </Navbar>
     )
   }
