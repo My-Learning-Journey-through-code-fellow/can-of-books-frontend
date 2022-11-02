647bbdad269375d2d71e54330000a4ab628572fb
Trace Dugar <tracedugar89@gmail.com>
2022-11-02 15:10:25 -0700

    update functionality added

--- src/components/BestBooks.js
+++ src/components/BestBooks.js
@@ -14,7 +14,8 @@ class BestBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: []
+      books: [],
+      booktoUpdate: {},
     }
   }
 
@@ -24,74 +25,76 @@ class BestBooks extends React.Component {
       this.setState({
         books: bookData.data,
       })
-      console.log('hello');
+      console.log(this.state.books);
     } catch (error) {
       console.log('We have an error.', error.response);
     }
   }
-//--------------- form submission-----------------------
-// handleBookSubmit = (e) => {
-//   e.preventDefault();
-//   // console.log(e.target.description.value)
-//   console.log(e.target.title.value)
-//   const newBook = {
-//     title: e.target.title.value,
-//     // location: e.target.location.value,
-//     description: e.target.description.value,
-//     status: e.target.status.checked
-//     // name: event.target.name.value,
-//     // spayNeuter: event.target.spayNeuter.checked,
-//   }
-//   console.log(newBook);
-//   this.postBooks(newBook);
-// }
+  //--------------- form submission-----------------------
+  // handleBookSubmit = (e) => {
+  //   e.preventDefault();
+  //   // console.log(e.target.description.value)
+  //   console.log(e.target.title.value)
+  //   const newBook = {
+  //     title: e.target.title.value,
+  //     // location: e.target.location.value,
+  //     description: e.target.description.value,
+  //     status: e.target.status.checked
+  //     // name: event.target.name.value,
+  //     // spayNeuter: event.target.spayNeuter.checked,
+  //   }
+  //   console.log(newBook);
+  //   this.postBooks(newBook);
+  // }
 
-postBooks = async (newBookObj) => {
-  try {
-    let url = `${process.env.REACT_APP_SERVER_URL}/books`;
-    let createdBook = await axios.post(url, newBookObj);
-    this.setState({
-      books: [...this.state.books, createdBook.data]
-    })
-  } catch (error) {
-    console.log(error.message);
+  postBooks = async (newBookObj) => {
+    try {
+      let url = `${process.env.REACT_APP_SERVER_URL}/books`;
+      let createdBook = await axios.post(url, newBookObj);
+      this.setState({
+        books: [...this.state.books, createdBook.data]
+      })
+    } catch (error) {
+      console.log(error.message);
+    }
   }
-}
 
-deleteBooks = async (bookToBeDeleted) => {
-  try {
-    let url = `${process.env.REACT_APP_SERVER_URL}/books/${bookToBeDeleted._id}`;
-    await axios.delete(url);
+  deleteBooks = async (bookToBeDeleted) => {
+    try {
+      let url = `${process.env.REACT_APP_SERVER_URL}/books/${bookToBeDeleted._id}`;
+      await axios.delete(url);
 
-    let updatedBooks = this.state.books.filter(book => book._id !== bookToBeDeleted._id);
-    this.setState({
-      books: updatedBooks
-    })
-  } catch (error) {
-    console.log(error.message);
+      let updatedBooks = this.state.books.filter(book => book._id !== bookToBeDeleted._id);
+      this.setState({
+        books: updatedBooks
+      })
+    } catch (error) {
+      console.log(error.message);
+    }
   }
-}
-//------------------------------------------------------
-updatedBooks = async (bookToUpdate) => {
-  try {
-    let url = `${process.env.REACT_APP_SERVER_URL}/books/${bookToUpdate._id}`
-    let updatedBook = await axios.put(url, bookToUpdate);
-    
-    let updatedBookArray = this.state.books.map(existingBook => {
-      return existingBook._id === bookToUpdate._id ? updatedBook.data : existingBook;
-    });
+  //------------------------------------------------------
+  updatedBooks = async (bookToUpdate) => {
+    console.log(bookToUpdate);
+    try {
+      let url = `${process.env.REACT_APP_SERVER_URL}/books/${bookToUpdate._id}`
+      let updatedBook = await axios.put(url, bookToUpdate);
 
-    this.setState({
-      books: updatedBookArray
-    });
+      let updatedBookArray = this.state.books.map(existingBook => {
 
-  } catch (error) {
-    console.log(error.message)
+        return existingBook._id === bookToUpdate._id ? updatedBook.data : existingBook;
+      });
+
+      this.setState({
+        books: updatedBookArray
+      });
+
+    } catch (error) {
+      console.log(error.message)
+    }
   }
-}
 
 
-//---------------------------------
+  //---------------------------------
 
   componentDidMount() {
     this.getBooks();
@@ -100,63 +103,67 @@ updatedBooks = async (bookToUpdate) => {
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
 
   render() {
-    
+
     console.log("App State >>>", this.state.books);
     /* TODO: render all the books in a Carousel */
 
     return (
       <>
-      <Container>
-       <BookFormModal
-       deleteBooks={this.deleteBooks}
-       postBooks={this.postBooks}
-       books={this.state.books}
-       />
-      </Container>
-      <Container>
-      <UpdateBookForm
-      updatebook={this.updatebook}
-      />
-      </Container>
-      <Container fluid id="bestBooksContainer">
-        {this.state.books.length > 0 ? 
-        
-        <Carousel variant='dark' loop={true} rows={1} cols={3} id="carousel">
-          {this.state.books.map((books, index) => (
-            <Carousel.Item style={{padding: 150}} key={index}  className="bookCarousel">
-                <Card className='bookCard'>
-                    <Button variant="dark" style={{width: 'max-content', margin: 'auto'}} onClick={() => {this.deleteBooks(books)}}>Delete</Button>
+        <Container>
+          <BookFormModal
+            deleteBooks={this.deleteBooks}
+            postBooks={this.postBooks}
+            books={this.state.books}
+          />
+        </Container>
+        {
+          this.state.showUpdateForm &&
+          <Container>
+            <UpdateBookForm
+              updateBooks={this.updatedBooks}
+              book={this.state.booktoUpdate}
+            />
+          </Container>
+        }
+        <Container fluid id="bestBooksContainer">
+          {this.state.books.length > 0 ?
 
-                    <Button variant="info" onClick={() => {this.setState({showUpdateForm: true })}}>Update</Button>
+            <Carousel variant='dark' loop={true} rows={1} cols={3} id="carousel">
+              {this.state.books.map((books, index) => (
+                <Carousel.Item style={{ padding: 150 }} key={index} className="bookCarousel">
+                  <Card className='bookCard'>
+                    <Button variant="dark" style={{ width: 'max-content', margin: 'auto' }} onClick={() => { this.deleteBooks(books) }}>Delete</Button>
 
-                  <Card.Body style={{width: 'max-content', margin: 'auto'}}>
-                    <Card.Title className="bookTitle">
-                      {" "}
-                      Title: {books.title}{" "}
-                    </Card.Title>
-                    <Card.Text className="bookDescription">
-                      Description: {books.description}
-                    </Card.Text>
-                    <img
-                      src={`https://placekitten.com/g/400/400`}
-                      alt={books.title}
-                      rounded="true"
-                      id="cardImg"
+                    <Button variant="info" onClick={() => { this.setState({ showUpdateForm: true, booktoUpdate: books }) }}>Update</Button>
+
+                    <Card.Body style={{ width: 'max-content', margin: 'auto' }}>
+                      <Card.Title className="bookTitle">
+                        {" "}
+                        Title: {books.title}{" "}
+                      </Card.Title>
+                      <Card.Text className="bookDescription">
+                        Description: {books.description}
+                      </Card.Text>
+                      <img
+                        src={`https://placekitten.com/g/400/400`}
+                        alt={books.title}
+                        rounded="true"
+                        id="cardImg"
                       />
-                    <Card.Text className="bookText">
-                    </Card.Text>
-                    <Card.Text className="bookText">
-                      Available: {books.status}
-                    </Card.Text>
-                    {/* <Button variant="dark" onClick={() => {this.deleteBooks(books)}}>Delete</Button> */}
-                  </Card.Body>
-                </Card>
-              </Carousel.Item>
-            ))}
-        </Carousel>
-        : <p>No books available</p>
-      }
-      </Container>
+                      <Card.Text className="bookText">
+                      </Card.Text>
+                      <Card.Text className="bookText">
+                        Available: {books.status}
+                      </Card.Text>
+                      {/* <Button variant="dark" onClick={() => {this.deleteBooks(books)}}>Delete</Button> */}
+                    </Card.Body>
+                  </Card>
+                </Carousel.Item>
+              ))}
+            </Carousel>
+            : <p>No books available</p>
+          }
+        </Container>
       </>
     )
   }
